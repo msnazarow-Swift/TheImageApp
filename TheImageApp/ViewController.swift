36130e537a8fdbc1eaef2b9bfa7ec700506cca44
msnazarow <msnazarow@gmail.com>
2021-08-06 04:42:32 +0300

    Initial Commit

---
+++ TheImageApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheImageApp
+//
+//  Created by Shelley Gertrudis on 8/6/21.
+//  Copyright © 2021 Shelley Gertrudis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
